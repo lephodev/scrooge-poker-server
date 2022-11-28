30bbecbf6aaafd2ae01bb7afcc15afde6b686c9b
ashrafwbst <er.ashrafkamal@gmail.com>
2022-11-28 15:51:13 +0530

    CORS issue

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const io = socket(server, {
   pingInterval: 10000,
   pingTimeout: 5000,
 });
-const whitelist = ['*'];
+const whitelist = ['http://localhost:3000', 'https://poker.scrooge.casino'];
 const corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
@@ -40,7 +40,7 @@ app.use(
     extended: false,
   })
 );
-app.use(cors());
+app.use(cors(corsOptions));
 mongoConnect();
 
 // Auth functions
