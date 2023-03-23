a34cf685608b06c88482f87918fb61d27465c1d8
RizwanAhmad-dev <rizwan@websultanate.com>
2023-03-23 18:57:28 +0530

    updated transactions

--- models/transaction.js
+++ models/transaction.js
@@ -1,12 +1,12 @@
 //imports
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
 //creating mongo database schema
 const transactionSchema = new Schema({
-  userId: { type: Schema.Types.ObjectId, ref: 'users' },
-  roomId: { type: Schema.Types.ObjectId, ref: 'room', default: null },
+  userId: { type: Schema.Types.ObjectId, ref: "users" },
+  roomId: { type: Schema.Types.ObjectId, ref: "room", default: null },
   amount: { type: Number },
   prevWallet: { type: Number },
   prevToken: { type: Number },
@@ -14,9 +14,14 @@ const transactionSchema = new Schema({
   updatedToken: { type: Number },
   transactionDetails: {},
   tournamentId: { type: String },
-  transactionType: { type: String, enum: ['poker', 'blackjack', 'slot','poker tournament'] },
+  transactionType: {
+    type: String,
+    enum: ["poker", "blackjack", "slot", "poker tournament"],
+  },
+  prevTicket: { type: Number },
+  updatedTicket: { type: Number },
 });
 
-const transactionModel = mongoose.model('transactions', transactionSchema);
+const transactionModel = mongoose.model("transactions", transactionSchema);
 
 export default transactionModel;
