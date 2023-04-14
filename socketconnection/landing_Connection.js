de90563c68a5c47fe6a8450ef3df3b05f1f3fccc
RizwanAhmad-dev <rizwan@websultanate.com>
2023-04-14 19:32:03 +0530

    updatedUnnecessary tables fetching on front end

--- socketconnection/landing_Connection.js
+++ socketconnection/landing_Connection.js
@@ -1,40 +1,42 @@
-import openSocket from 'socket.io-client';
-import { getAllGame } from '../controller/tournamentController';
-import tournamentModel from '../models/tournament';
+import openSocket from "socket.io-client";
+import { getAllGame } from "../controller/tournamentController";
+import tournamentModel from "../models/tournament";
 //https://dev-api.scrooge.casino
 //http://localhost:3004
 
 const connetToLanding = (pokerSocket) => {
-    const socket = openSocket(process.env.LANDING_API_URL, {
-        transports: ['websocket'],
-        rejectUnauthorized: false,
-    });
-    socket.on('connect', () => {
-        console.log('conected to landing server server');
-    });
+  const socket = openSocket(process.env.LANDING_API_URL, {
+    transports: ["websocket"],
+    rejectUnauthorized: false,
+  });
+  socket.on("connect", () => {
+    console.log("conected to landing server server");
+  });
 
-    socket.on('tournamentAction', async(data) => {
-         const getAllTournament = await tournamentModel.find({}).populate("rooms");
-         console.log("tournament created at landing");
-         pokerSocket.emit('tournamentCreated', { tournaments: getAllTournament }); 
-    })
-    socket.on('tableCreate', async(data) => {
-        pokerSocket.emit("AllTables",{tables:data?.tables}) 
-   })
-    
-    const tryReconnect = () => {
-        setTimeout(() => {
-            socket.io.open((err) => {
-                if (err) {
-                    tryReconnect();
-                }
-            });
-        }, 2000);
-    };
-    
-    socket.io.on('close', tryReconnect);
-    return socket;
-}
+  socket.on("tournamentAction", async (data) => {
+    const getAllTournament = await tournamentModel.find({}).populate("rooms");
+    console.log("tournament created at landing");
+    pokerSocket.emit("tournamentCreated", { tournaments: getAllTournament });
+  });
+  socket.on("tableCreate", async (data) => {
+    let reqTables = data?.tables?.filter((el) => {
+      return el.public === true && el.finish === false && gameType === "poker";
+    });
+    pokerSocket.emit("AllTables", { tables: reqTables });
+  });
 
+  const tryReconnect = () => {
+    setTimeout(() => {
+      socket.io.open((err) => {
+        if (err) {
+          tryReconnect();
+        }
+      });
+    }, 2000);
+  };
+
+  socket.io.on("close", tryReconnect);
+  return socket;
+};
 
 export { connetToLanding };
