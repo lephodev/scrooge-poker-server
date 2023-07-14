c301136686d26fc81ab593de3ca11882f89cdfac
RizwanAhmad-dev <rizwan@websultanate.com>
2023-07-14 14:06:16 +0530

    implemented spectate for the multi-table tournament

--- cron/cron.js
+++ cron/cron.js
@@ -6,10 +6,11 @@ import { CronJob } from "cron";
 import { ISO_8601 } from "moment/moment";
 import { activateTournament } from "../functions/functions";
 import gameService from "../service/game.service";
-const returnCron = async(io) => {
+const returnCron = async (io) => {
   const job1 = new CronJob("*    *    *    *    *", async () => {
-    await gameService.sendAcknowledgementForJoinTournament(io)
-    await activateTournament(io)
+    await gameService.sendAcknowledgementForJoinTournament(io);
+    await activateTournament(io);
+    await gameService.checkJoinTimeExceeded(io);
   });
   job1.start();
 };
